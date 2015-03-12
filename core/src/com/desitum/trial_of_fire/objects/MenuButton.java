486597208cec5172ab54cdcb56f3af4cb69544dc
DuhProgrammer13 <duhvanry@gmail.com>
2015-03-12 13:41:29 -0600

    Starting

---
+++ core/src/com/desitum/trial_of_fire/objects/MenuButton.java
@@ -0,0 +1,22 @@
+package com.desitum.trial_of_fire.objects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by dvan6234 on 3/12/2015.
+ */
+public class MenuButton extends Sprite {
+
+    private String command;
+
+    public MenuButton(Texture texture, String command, float sizeX, float sizeY, float x, float y){
+        super(texture, 0, 0, texture.getWidth(), texture.getHeight());
+        setSize(sizeX, sizeY);
+        this.command = command;
+    }
+
+    public String getCommand(){
+        return command;
+    }
+}
--- core/src/com/desitum/trial_of_fire/screens/GameScreen.java
+++ core/src/com/desitum/trial_of_fire/screens/GameScreen.java
@@ -10,11 +10,11 @@ public class GameScreen {
     private TrialofFire gameToF;
 
     public static int state;
-    public static final int STORY;
-    public static final int IN_BATTLE;
-    public static final int CONVERSATION;
-    public static final int GAME_PLAY;
-    public static final int PAUSED;
+    public static final int STORY = 0;
+    public static final int IN_BATTLE = 1;
+    public static final int CONVERSATION = 2;
+    public static final int GAME_PLAY = 3;
+    public static final int PAUSED = 4;
 
     public GameScreen(TrialofFire game){
         gameToF = game;
