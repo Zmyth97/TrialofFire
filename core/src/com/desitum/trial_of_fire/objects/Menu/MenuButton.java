a2975ac245c7fc7fd13997f58d9f5aa3906437e0
DuhProgrammer13 <duhvanry@gmail.com>
2015-03-26 13:40:42 -0600

    Starting

--- core/src/com/desitum/trial_of_fire/objects/Menu/MenuButton.java
+++ core/src/com/desitum/trial_of_fire/objects/Menu/MenuButton.java
@@ -2,6 +2,7 @@ package com.desitum.trial_of_fire.objects.Menu;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector3;
 
 /**
  * Created by dvan6234 on 3/12/2015.
@@ -14,10 +15,20 @@ public class MenuButton extends Sprite {
 
     public MenuButton(Texture texture, Texture activeTexture, String command, float sizeX, float sizeY, float x, float y){
         super(texture, 0, 0, texture.getWidth(), texture.getHeight());
+        baseTexture = texture;
+        activeTexture = texture;
         setSize(sizeX, sizeY);
         this.setX(x);
         this.setY(y);
         this.command = command;
+    }
+
+    public void update(float delta){
+
+    }
+
+    public void updateTouch(Vector3 touchPoint){
+
     }
 
     public String getCommand(){
--- core/src/com/desitum/trial_of_fire/world/MenuWorld.java
+++ core/src/com/desitum/trial_of_fire/world/MenuWorld.java
@@ -1,7 +1,13 @@
 package com.desitum.trial_of_fire.world;
 
+import com.desitum.trial_of_fire.objects.Menu.MenuButton;
+
+import java.util.ArrayList;
+
 /**
  * Created by Zmyth97 on 3/10/2015.
  */
 public class MenuWorld {
+
+    private ArrayList<MenuButton>
 }
